48815d5a9fd0bd7fe2aefa8d63ef3b78fdb55fc6
anuoluwapo <oluwapoanu87@gmail.com>
2023-08-23 07:10:39 +0100

    Chore: build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aefff416a4c04ef0"; }
+/******/ 		__webpack_require__.h = function() { return "fc72a1d7bd69e285"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
