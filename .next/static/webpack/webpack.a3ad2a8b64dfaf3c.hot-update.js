2ed882ebaf9b9ce9637d3ca6abc9fe7ec7bfdfa7
anuoluwapo <oluwapoanu87@gmail.com>
2023-10-06 18:40:44 +0100

    Chore: Fixed Bugs

--- .next/static/webpack/webpack.a3ad2a8b64dfaf3c.hot-update.js
+++ .next/static/webpack/webpack.a3ad2a8b64dfaf3c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ec89cac8b9687908"; }
+/******/ 	__webpack_require__.h = function() { return "521b314410d5b8f9"; }
 /******/ }();
 /******/ 
 /******/ }
